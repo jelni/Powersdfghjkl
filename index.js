37583d1bdf1ba786864a2ab5345b1d8bb7459ef5
jel <25802745+jelni@users.noreply.github.com>
2021-03-14 20:25:29 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const { Plugin } = require('powercord/entities');
+const all_letters = ['SDFGJKL', 'WERUIAHVB', 'QTYOPZXCNM'];
+
+module.exports = class Sdfghjkl extends Plugin {
+	startPlugin() {
+		powercord.api.commands.registerCommand({
+			command: 'sdfghjkl',
+			description: 'UEYWASRFGWEYUJSGTFUWJKARFHDUKQWGFCUKWAJEGFDUKQWAHDKQWAHGKAQWDHUAWHAWHJKDHKfawGHQKJRFDwahKJIDRFQjkfGHASEkjFKJAWHJHSGAEDHFDjqkashkfvahkadfhjsd',
+			usage: '{c} [length]',
+			executor: (args) => this.sdfghjkl(args)
+		});
+	}
+
+	pluginWillUnload() {
+		powercord.api.commands.unregisterCommand('sdfghjkl');
+	}
+
+	sdfghjkl(args) {
+		let lower = args.length >= 2 && args[1].toLowerCase() === 'l';
+
+		let length;
+
+		if (args.length >= 1) {
+			length = parseInt(args[0]);
+			if (isNaN(length)) return;
+			Math.min(Math.max(length, 1), 2000);
+		} else {
+			length = Math.floor(Math.random() * 51 + 50);
+		}
+
+		let result = '';
+
+		for (let i = 0; i < length; i++) {
+			let letters;
+			let n = Math.random();
+
+			if (n >= 0.4) letters = all_letters[0];
+			else if (n >= 0.1) letters = all_letters[1];
+			else letters = all_letters[2];
+
+			result += letters.charAt(Math.floor(Math.random() * letters.length));
+		}
+
+		if (lower) {
+			result = result.toLowerCase();
+		}
+
+		return {
+			send: true,
+			result: result
+		};
+	}
+};
